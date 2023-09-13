74d60c63256efdb1da4d49159ebeb08e61a6e703
Ivan Draganov <dev.advantech@gmail.com>
2023-09-13 16:56:16 +0300

    initialize the tasks

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<h1>Task manager </h1>
+
+<div>
+    @forelse ($tasks as $task)
+    <div>
+        <a href={{ route('tasks.show', ['id' => $task->id]) }}>{{ $task->title }}</a>
+    </div>
+    @empty
+    <div>
+        There are no tasks!
+    </div>
+    @endforelse
+</div>
